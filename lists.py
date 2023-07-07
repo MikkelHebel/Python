0ffc010c215b3dfed403d41a5b013f523aae90ce
MikkelHebel <mikkelhebelv2@gmail.com>
2023-07-07 14:05:32 +0200

    Updated lists.py

--- lists.py
+++ lists.py
@@ -1,3 +1,23 @@
 animals = ['cat', 'rat', 'bat', 'elephant', 'horse']
-
 print(animals[0:3])
+
+
+lists = ['cat', 'dog'], [10, 20, 30, 40, 50, 100]
+print(lists[0][1])
+# Print 50
+print(lists[1][4])
+
+# Setting new value in list
+lists[1][4] = 500
+# Print 500
+print(lists[1][4])
+
+
+# Get last line in list 2
+print(lists[1][-1])
+
+
+
+# Grab all the values from the beginning to value 10
+numberList = [10, 20, 30, 40, 50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
+print(numberList[:10])
