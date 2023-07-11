2156900fb1353525f2487c286bfc09880b0a9727
MikkelHebel <mikkelhebelv2@gmail.com>
2023-07-11 15:02:42 +0200

    Added cat to the list

--- lists.py
+++ lists.py
@@ -28,7 +28,13 @@ if 'heyas' in ['hello', 'test', 'world', 'heyas']:
     print('Found it!')
 
 
-# List of suppliesg
+# List of supplies
 supplies = ['pens', 'staplers', 'flame-throwers', 'binders']
 for i in range(len(supplies)):
     print('Index ' + str(i) + ' in supplies is: ' + supplies[i])
+
+cat = ['fat', 'orange', 'loud']
+size, color, disposition = cat
+print(size)
+print(color)
+print(disposition)
