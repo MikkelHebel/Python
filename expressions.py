2bb2b8b766ac50b2efa48c24bb887c6443a1b4b7
MikkelHebel <mikkelhebelv2@gmail.com>
2023-07-18 14:58:04 +0200

    modified expressions.py

--- expressions.py
+++ expressions.py
@@ -7,5 +7,4 @@ message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office.'
 phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
 mo = phoneNumRegex.search(message)
 
-print(mo.group())
 print(phoneNumRegex.findall(message))
